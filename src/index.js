71f2e858785e56e27829e42186747802b80fc1e3
steviebuckets <steviebuckets33@gmail.com>
2017-09-10 20:45:48 -0700

    testing

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('rot'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
