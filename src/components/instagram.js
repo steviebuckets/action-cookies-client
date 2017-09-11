6500a9911f476e607d2ec7057e2d96eec0f4631d
steviebuckets <steviebuckets33@gmail.com>
2017-09-10 19:03:51 -0700

    updated insta widget

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -5,14 +5,33 @@ export default class Instagram extends React.Component {
   render() {
     return (
       <div className="insta-container">
-        <iframe title="title" src="//lightwidget.com/widgets/1c1e842401965a598386f8656515ea35.html" scrolling="no" allowTransparency="true" className="lightwidget-widget" style={{
-          width: "100%",
-          border: "0",
-          opacity: "0.8",
-          overflow: "hidden"
-        }}></iframe>
-
+      <iframe title="title" src="//lightwidget.com/widgets/8b9284a197d255fea377257ba4e5a0c9.html" scrolling="no" allowtransparency="true" className="lightwidget-widget"
+        style={{
+        width: "100%",
+         border: "0",
+         opacity: "0.8",
+        overflow: "hidden"
+      }}></iframe>
       </div>
     )
   }
 }
+
+
+
+
+// Paid Insta Code
+// <script src="//lightwidget.com/widgets/lightwidget.js"></script>
+// <iframe src="//lightwidget.com/widgets/8b9284a197d255fea377257ba4e5a0c9.html" scrolling="no" allowtransparency="true"
+// class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+
+
+
+// Original Code for Insta Widget - not paid
+// <iframe title="title" src="//lightwidget.com/widgets/1c1e842401965a598386f8656515ea35.html" scrolling="no" allowTransparency="true" className="lightwidget-widget"
+// style={{
+//   width: "100%",
+//   border: "0",
+//   opacity: "0.8",
+//   overflow: "hidden"
+// }}></iframe>
